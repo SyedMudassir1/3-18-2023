6bea4aea1a477ec765a6cb23bbf5206d13c44f33
SyedMudassir1 <122387591+SyedMudassir1@users.noreply.github.com>
2023-03-18 03:10:29 -0700

    Add files via upload

---
+++ Jawascript/app.js
@@ -0,0 +1,103 @@
+
+// var headuser = prompt("enter head username");
+// var tailsuser = prompt("enter tails username");
+
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss)
+
+// if(floor === 0){
+//   alert("Heads " + headuser + " win the toss")
+// }else{
+//   alert("tails " + tailsuser + " win the toss")
+// }
+
+
+//  var headuser = prompt("enter head username");
+//    var tailsuser = prompt("enter tails username");
+
+//    var toss = Math.random() * 2;
+//    var floor = Math.floor(toss)
+
+//    if(floor === 0){
+//     alert("Head " + headuser + " win the toss")
+//    }else{
+//     alert("Tails " + tailsuser + " win the toss")
+//    }
+
+// var headuser = prompt("enter heads username");
+// var tailsuser = prompt("enter tails username");
+
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss)
+
+
+// if(floor === 0){
+//   alert("Head " + headuser + " win the toss");
+// }else{
+//   alert("Tails " + tailsuser + " win the toss");
+// }
+
+// var headuser = prompt("enter your head username");
+// var tailsuser = prompt("enter your tails username");
+
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss);
+ 
+// if(floor === 0){
+//   alert("Head " + headuser + " win the toss")
+// }else{
+//   alert("tails " + tailsuser + " win the toss")
+// }
+
+//  var headuser = prompt("enter heads username");
+
+// var tailsuser = prompt("enter tails username");
+
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss)
+
+
+// if(floor === 0){
+//   alert("Head " + headuser + " win the toss");
+// }else{
+//   alert("Tails " + tailsuser + " win the toss");
+// }
+
+// var headuser = prompt("enter head username");
+// var tailsuser = prompt("enter tails username");
+
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss);
+
+// if(floor === 0){
+//   alert("Head " + headuser + " win the toss")
+// }else{
+//   alert("tails " + tailsuser + " win the toss")
+// }
+ 
+// var headuser = prompt("enter head username");
+// var tailsuser = prompt("enter tails username");
+
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss)
+
+// if(floor === 0){
+//   alert("Head " + headuser + " win the toss")
+// }else{
+//   alert("Tails " + tailsuser + " win the toss")
+// }
+
+// var headuser = prompt("enter head username");
+// var tailsuser = prompt("enter tails username");
+
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss);
+
+// if(floor === 0){
+//   alert("Head " + headuser + " win the toss");
+// }else{
+//   alert("Tails " + tailsuser + " win the toss");
+// }
+
+// var headuser = prompt("enter head username");
+// var tailsuser = prompt("enter tails username");
